a81cacd2fca297ee12475f792bc2ddab4c1a8e39
Christian Sadilek <christian.sadilek@gmail.com>
2017-04-03 17:36:41 -0400

    Add functionality to generate and verify API keys

--- config/config.go
+++ config/config.go
@@ -11,32 +11,33 @@ import (
 
 // Config holds all system-wide settings
 type Config struct {
-	secret              string
-	serverAddr          string
-	serverContentPath   string
-	serverImportPath    string
-	importQueueDir      string
-	indexDir            string
-	indexFile           string
-	providerRegistryDir string
+	Secret              string
+	ServerAddr          string
+	ServerContentPath   string
+	ServerImportPath    string
+	ImportQueueDir      string
+	IndexDir            string
+	IndexFile           string
+	ProviderRegistryDir string
 }
 
 // Get returns the configuration based on config.toml, if present.
 // Default values are provided for all keys not present, except Secret.
 func Get() *Config {
 	c := &Config{
-		serverAddr:          ":8080",
-		serverContentPath:   "/crec/content",
-		serverImportPath:    "/crec/import",
-		importQueueDir:      "import",
-		indexDir:            "index",
-		indexFile:           "crec.bleve",
-		providerRegistryDir: "crec-registry"}
+		ServerAddr:          ":8080",
+		ServerContentPath:   "/crec/content",
+		ServerImportPath:    "/crec/import",
+		ImportQueueDir:      "import",
+		IndexDir:            "index",
+		IndexFile:           "crec.bleve",
+		ProviderRegistryDir: "crec-registry"}
 
 	bytes, err := ioutil.ReadFile(filepath.FromSlash("config.toml"))
 	if err != nil {
 		return c
 	}
+
 	_, err = toml.Decode(string(bytes), &c)
 	if err != nil {
 		log.Println("Failed to read provided config (using default settings): ", err)
@@ -47,40 +48,40 @@ func Get() *Config {
 
 // GetAddr returns the address and port for starting up the server .e.g :8080
 func (c *Config) GetAddr() string {
-	return c.serverAddr
+	return c.ServerAddr
 }
 
 // GetContentPath returns the URL path to handle content requests e.g. /crec/content
 func (c *Config) GetContentPath() string {
-	return c.serverContentPath
+	return c.ServerContentPath
 }
 
 // GetImportPath returns the URL path to handle import requests e.g. /crec/import
 func (c *Config) GetImportPath() string {
-	return c.serverImportPath
+	return c.ServerImportPath
 }
 
 // GetImportQueueDir returns the directory path to store imported content e.g. import
 func (c *Config) GetImportQueueDir() string {
-	return c.importQueueDir
+	return c.ImportQueueDir
 }
 
 // GetIndexDir returns the directory path to store indexes e.g. index
 func (c *Config) GetIndexDir() string {
-	return c.indexDir
+	return c.IndexDir
 }
 
 // GetIndexFile returns the path to store the index e.g. crec.bleve
 func (c *Config) GetIndexFile() string {
-	return c.indexDir
+	return c.IndexDir
 }
 
 // GetProviderRegistryDir returns the directy path containing provider configurations e.g. crec-registry
 func (c *Config) GetProviderRegistryDir() string {
-	return c.providerRegistryDir
+	return c.ProviderRegistryDir
 }
 
 // GetSecret returns the configured secret to generate API keys
 func (c *Config) GetSecret() string {
-	return c.secret
+	return c.Secret
 }
---
+++ server/api.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+
+	"log"
+
+	"mozilla.org/crec/config"
+)
+
+// GenerateAPIKey returns an API key for the provider
+func GenerateAPIKey(provider string, config *config.Config) string {
+	plaintext := []byte(provider)
+
+	block, err := aes.NewCipher([]byte(config.GetSecret()))
+	if err != nil {
+		log.Fatal("Failed to generate API key for provider: "+provider+" ", err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		log.Fatal("Failed to generate API key for provider: " + provider)
+	}
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+// GetProviderForAPIKey returns the provider the key was generate for
+func GetProviderForAPIKey(key string, config *config.Config) string {
+	apikey, err := base64.URLEncoding.DecodeString(key)
+	if err != nil {
+		log.Fatal("Failed to decode API Key ", err)
+	}
+	block, err := aes.NewCipher([]byte(config.GetSecret()))
+	if err != nil {
+		log.Fatal("Failed to verify API Key ", err)
+	}
+	iv := apikey[:aes.BlockSize]
+	provider := apikey[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(provider, provider)
+	return string(provider)
+}
